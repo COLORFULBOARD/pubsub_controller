c99a5a5cf149a4c7bec38231bcc0275fc4ba3408
Hiroki Nakajima <h-nakajima@colorful-board.com>
2018-03-23 07:57:40 +0900

    setup.py restore

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+import re
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+try:
+    with open('LICENSE') as f:
+        license = f.read()
+except IOError:
+    license = ''
+
+with open('apps/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+            fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='pubsub_controller',
+    version=version,
+    description='It provides a resident process that periodically pull subscribes and publish on CLI and Python.',
+    long_description=readme,
+    author='SENSY Inc.',
+    url='https://github.com/COLORFULBOARD/pubsub_controller',
+    license=license,
+    install_requires=[
+        'google-cloud-pubsub==0.30.1'
+    ],
+    packages=find_packages()
+)
